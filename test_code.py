b6ed3b7e0fbb320714599679297e043ff9665d64
Abdul Wasay Khan <115465256+1wasay1@users.noreply.github.com>
2026-03-11 12:25:58 +0500

    Create test_code.py

---
+++ test_code.py
@@ -0,0 +1,25 @@
+import math
+
+def calculate_average(numbers):
+    total = 0
+    for i in range(len(numbers)):
+        total = total + numbers[i]
+    avg = total / len(numbers)
+    return avg
+
+def find_max(numbers):
+    max = numbers[0]
+    for i in numbers:
+        if i > max:
+            max = i
+    return max
+
+def unused_function():
+    x = 10
+    y = 20
+    z = x + y
+
+numbers = [10,20,30,40]
+
+print("Average:", calculate_average(numbers))
+print("Max:", find_max(numbers))
